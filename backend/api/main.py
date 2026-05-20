4a005e668cfc1344e13b8456e87668f619e3673a
LAPTOP-DGB2DLJ9\Arun <arunram.in@gmail.com>
2026-05-20 00:48:37 -0700

    minor update

--- backend/api/main.py
+++ backend/api/main.py
@@ -32,8 +32,9 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:5173",
     "http://localhost:3000",
-    "https://sigmamedstat-api.fly.dev",
-    ],
+    "https://crip-x-cqoo.vercel.app",
+    "https://sigmamedstat.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
